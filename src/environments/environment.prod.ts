68d66e4e584e6e3951a9730337a0277aa7cf5df8
Abdul Azis <abdul.azis@agungsedayu.com>
2018-03-05 02:09:23 +0700

    change url to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL:'https://young-eyrie-51496.herokuapp.com',
+  //BASE_URL:'https://young-eyrie-51496.herokuapp.com',
+  BASE_URL:'https://rw03-greengarden.herokuapp.com',
   token:'b156eb3d1c48875e967a7322cbfdc850ff31642a'
 };
